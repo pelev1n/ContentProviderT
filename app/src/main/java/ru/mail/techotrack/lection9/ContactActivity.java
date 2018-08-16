54f2bc94119ba19eecd51d358a5278216da5452e
pelev1n <pelevin9911@gmail.com>
2018-08-17 02:06:21 +0300

    Added contact activity

---
+++ app/src/main/java/ru/mail/techotrack/lection9/ContactActivity.java
@@ -0,0 +1,15 @@
+package ru.mail.techotrack.lection9;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class ContactActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R);
+    }
+}
